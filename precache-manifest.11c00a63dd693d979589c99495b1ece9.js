94e4d1257f68634aee48e12f44c9ad7ef420f99c
Grievous Angel <ronfud2000@gmail.com>
2020-01-04 13:37:42 -0500

    Updates

---
+++ precache-manifest.11c00a63dd693d979589c99495b1ece9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "818b436eb9bb51a329e1eefba29cbeb0",
+    "url": "/clicky-game/index.html"
+  },
+  {
+    "revision": "a28cc9eb0003e5e261c4",
+    "url": "/clicky-game/static/css/main.5f8ea3b7.chunk.css"
+  },
+  {
+    "revision": "73026816a37617a2987b",
+    "url": "/clicky-game/static/js/2.b094a780.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/clicky-game/static/js/2.b094a780.chunk.js.LICENSE"
+  },
+  {
+    "revision": "a28cc9eb0003e5e261c4",
+    "url": "/clicky-game/static/js/main.8d3812d7.chunk.js"
+  },
+  {
+    "revision": "b5e3d5480cb174bedaf8",
+    "url": "/clicky-game/static/js/runtime-main.dbb64346.js"
+  }
+]);
