68fef39d856fbbc1bca907719f9ac58b837793fb
Grievous Angel <ronfud2000@gmail.com>
2020-01-09 18:08:18 -0500

    fixed image backgrounds

--- src/components/Instructions/index.js
+++ src/components/Instructions/index.js
@@ -3,11 +3,15 @@ import "./style.css";
 
 function Instructions(props) {
 //   return (<h3 className="instructions">{props.children}</h3>)
-  return <h3 className="instructions">Test your memory.<br></br>You win by clicking on
-   each of the images only once.<br></br>Your Current Score increases by one for each 
-   click.<br></br> If you click on an image twice you lose.<br></br> You can start a 
-   new game by clicking on an image.<br></br>Your Top Score is 
-   displayed until the page is exited of refreshed.</h3>
+  return (
+  <div>
+    <h2 className="instructions">Test your memory.</h2>
+    <h3>You win by clicking on each of the images only once.<br></br>
+    Your Current Score increases by one for each click.<br></br>
+    If you click on an image twice you lose.<br></br> You can start a 
+   new game by clicking on an image.<br></br>
+   Your Top Score is displayed until the page is exited of refreshed.</h3>
+   </div>)
 }
 
 export default Instructions;
